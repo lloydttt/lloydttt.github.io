b2ef0ee98368299c9a309b4bce5a4bda3f029011
lloydttt <126597991+lloydttt@users.noreply.github.com>
2023-05-31 18:52:09 +0800

    Site updated: 2023-05-31 18:52:09

--- EveryDayEn/index.js
+++ EveryDayEn/index.js
@@ -1,14 +1,31 @@
+// const url = 'https://api.vvhan.com/api/en';
+
+// fetch(url)
+//   .then(response => response.json())
+//   .then(data => {
+//     const quote = `${data.data.en} - ${data.data.zh}`; // 组合英文和中文名言
+//     const picUrl = data.data.pic; // 图片链接
+
+//     document.getElementById('quote').textContent = quote; // 更新名言文本
+//     document.getElementById('pic').src = picUrl; // 更新图片链接
+//   })
+//   .catch(error => console.error(error));
+
+
+
 const url = 'https://api.vvhan.com/api/en';
 
 fetch(url)
   .then(response => response.json())
   .then(data => {
-    const quote = `${data.data.en} - ${data.data.zh}`; // 组合英文和中文名言
+    const quoteEn = data.data.en; // 英文名言
+    const quoteZh = data.data.zh; // 中文名言
     const picUrl = data.data.pic; // 图片链接
 
-    document.getElementById('quote').textContent = quote; // 更新名言文本
+    document.getElementById('quote-en').textContent = quoteEn; // 更新英文名言
+    document.getElementById('quote-zh').textContent = quoteZh; // 更新中文名言
     document.getElementById('pic').src = picUrl; // 更新图片链接
   })
   .catch(error => console.error(error));
 
-  
+
