d58434ff4654e125f66f2732a8f245f59c606bc9
lloydttt <126597991+lloydttt@users.noreply.github.com>
2023-05-31 08:28:18 +0800

    Site updated: 2023-05-31 08:28:18

---
+++ EveryDayEn/index.js
@@ -0,0 +1,14 @@
+const url = 'https://api.vvhan.com/api/en';
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    const quote = `${data.data.en} - ${data.data.zh}`; // 组合英文和中文名言
+    const picUrl = data.data.pic; // 图片链接
+
+    document.getElementById('quote').textContent = quote; // 更新名言文本
+    document.getElementById('pic').src = picUrl; // 更新图片链接
+  })
+  .catch(error => console.error(error));
+
+  
